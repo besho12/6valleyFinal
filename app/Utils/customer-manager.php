21d4c9e8fcb7901aa4c33b09196333ab18b11db2
Beshoy <beshoy.ecladuos@onexpand.co.uk>
2025-03-05 21:12:53 +0200

    first commit

--- app/Utils/customer-manager.php
+++ app/Utils/customer-manager.php
@@ -168,16 +168,6 @@ class CustomerManager
         $debit = 0;
         $user = User::find($user_id);
 
-
-        try {
-            $ads_point_transaction = new AdsPointsTransaction();
-        } catch (\Throwable $ex) {
-            dd($ex->getMessage());
-        }
-
-        dd('hhhhhhhhhh');
-
-        
         $ads_point_transaction = new AdsPointsTransaction();
         $ads_point_transaction->user_id = $user->id;
         $ads_point_transaction->transaction_id = \Str::uuid();
