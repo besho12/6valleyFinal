149f5390623f5a3dbc84cf24453c39a866ed669a
Beshoy <beshoy.ecladuos@onexpand.co.uk>
2025-03-18 22:33:37 +0200

    adding new button for view all

--- resources/views/admin-views/business-settings/special-ads-settings.blade.php
+++ resources/views/admin-views/business-settings/special-ads-settings.blade.php
@@ -120,60 +120,32 @@
     console.log('This script is added via @push in Blade.');
 
     $(document).ready(function(){
-        console.log('OOOOOOOOO');
-        $('.delete-specialads').on('click', function () {
-            console.log('BBBBBBBBB');
-            let specialadsId = $(this).attr("id");
-            Swal.fire({
-                title: messageAreYouSureDeleteThis,
-                text: messageYouWillNotAbleRevertThis,
-                type: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: getYesWord,
-                cancelButtonText: getCancelWord,
-                reverseButtons: true
-            }).then((result) => {
-                if (result.value) {           
-                    $('#specialadsdeleteid').val(specialadsId);
-                    $('.delete-specialads-form-submit').submit();            
-                }
-            })
-        });
+        // console.log('OOOOOOOOO');
+        // $('.delete-specialads').on('click', function () {
+        //     console.log('BBBBBBBBB');
+        //     let specialadsId = $(this).attr("id");
+        //     Swal.fire({
+        //         title: messageAreYouSureDeleteThis,
+        //         text: messageYouWillNotAbleRevertThis,
+        //         type: 'warning',
+        //         showCancelButton: true,
+        //         confirmButtonColor: '#3085d6',
+        //         cancelButtonColor: '#d33',
+        //         confirmButtonText: getYesWord,
+        //         cancelButtonText: getCancelWord,
+        //         reverseButtons: true
+        //     }).then((result) => {
+        //         if (result.value) {           
+        //             $('#specialadsdeleteid').val(specialadsId);
+        //             $('.delete-specialads-form-submit').submit();            
+        //         }
+        //     })
+        // });
     })
 </script>
 @endpush
 
 @push('script_2')
-
-{{-- <script>
-    console.log('This script is added via @push in Blade.');
-
-    $(document).ready(function(){
-        console.log('OOOOOOOOO');
-        $('.delete-specialads').on('click', function () {
-            console.log('BBBBBBBBB');
-            let specialadsId = $(this).attr("id");
-            Swal.fire({
-                title: messageAreYouSureDeleteThis,
-                text: messageYouWillNotAbleRevertThis,
-                type: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: getYesWord,
-                cancelButtonText: getCancelWord,
-                reverseButtons: true
-            }).then((result) => {
-                if (result.value) {           
-                    $('#specialadsdeleteid').val(specialadsId);
-                    $('.delete-specialads-form-submit').submit();            
-                }
-            })
-        });
-    })
-</script> --}}
     <script src="{{dynamicAsset(path: 'public/assets/back-end/js/admin/business-setting/business-setting.js')}}"></script>
 @endpush
 
