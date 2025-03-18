c80c74646924866572efe302bb856644270ef05f
Beshoy <beshoy.ecladuos@onexpand.co.uk>
2025-03-18 23:51:34 +0200

    adding new button for view all

--- routes/rest_api/v1/api.php
+++ routes/rest_api/v1/api.php
@@ -394,6 +394,7 @@ Route::group(['namespace' => 'RestAPI\v1', 'prefix' => 'v1', 'middleware' => ['a
     // Route::post('add-ads-points', 'CustomerController@add_ads_points')->middleware('auth:api');
     Route::post('add-ads-points',  'CustomerController@add_ads_points')->middleware('auth:api');
     Route::post('get-ads-points',  'CustomerController@get_ads_points')->middleware('auth:api');
-    Route::post('get-special-ads', 'CustomerController@get_special_ads')->middleware('auth:api');
+    Route::post('get-special-ads', 'CustomerController@get_special_ads');
+    
 
 });
